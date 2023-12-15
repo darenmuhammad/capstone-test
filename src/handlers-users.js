d1af5bdc4b26af37c3d197cb69b6d3c805213580
Daren Muhammad <darenmuhammad10@gmail.com>
2023-12-15 14:40:43 +0700

    change bcrypt to bcryptjs

--- src/handlers-users.js
+++ src/handlers-users.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Joi = require('@hapi/joi');
 const { Users, insertUser, isUsernameExist } = require('../models/users');
 
